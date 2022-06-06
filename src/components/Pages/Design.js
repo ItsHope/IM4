583b8feaee6282688a940ed161c1b8c1ba5e8828
ItsHope <1918429@students.wits.ac.za>
2022-06-06 22:34:07 +0200

    submit

--- src/components/Pages/Design.js
+++ src/components/Pages/Design.js
@@ -1,8 +1,35 @@
 import React from 'react'
 
 const Design = () => {
-    return (
-        <div>DESIGN</div>
+    return ( 
+        <>
+        <div>
+            <img src={require('../Img/Homepage.png')} height={400} width={600}/>
+        </div>
+        <br></br>
+
+<div>
+<img src={require('../Img/BP.png')} height={400} width={600}/>
+</div>
+<br></br>
+
+<div>
+<img src={require('../Img/DP.png')} height={400} width={600}/>
+</div> <br></br>
+<div>
+<img src={require('../Img/WAW.png')} height={400} width={600}/>
+</div>
+
+<br></br>
+<div>
+<img src={require('../Img/Colourway.png')} height={400} width={600}/>
+</div>
+
+
+
+
+
+       </>
     )
 }
 
--- src/components/Pages/Tabs.js
+++ src/components/Pages/Tabs.js
@@ -100,27 +100,71 @@ function Tabs() {
           <div
             className={toggleState === 2 ? "content  active-content" : "content"}
           >
-            <h2>Content 2</h2>
+            <h2>Assignment 1 Reflection. </h2>
             <hr />
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-              voluptatum qui adipisci.
+            Through this assignment, I was first introduced to React and JSX. It was challenging for me to do the technical aspect of the assignment and I realized that I had not spent enough time preparing for the assignment. A react website is very different from a normal website and the rules are different. I do realize that I did not use the privilege of consulting with my tutors when there was still time. 
             </p>
+
+            <p> 
+            Although I created my website and coded for it, styled it and etc. I was not acknowledging “react rules” and therefore was unable to get some functionalities. For example, inserting a picture or creating a background image in a normal html website is done in the css file while inserting one in a react website would be done in a jsx file with back ticks that were also new to me too. 
+            </p>
+             <p>
+             I quickly had to learn these new things, hooks, using the terminal and etc. I had always pushed my code to GitHub from the GitHub application (GitHub Desktop) from my pc and never needed to use the terminal before now. This was a lot of new information for me all at one with little time. I attempted to learn but I ended up creating more errors while simply trying to deploy my website to GitHub pages. As a result I neglected the theory part of the assignment because my website wasn’t appearing although I was coding for it. As a result I never managed to submit. 
+             </p>  
+             <p>
+             In conclusion assignment one was my first attempt with react, I planned to do better with assignment 2. 
+             </p>
+
+             <h2>Assignement 2 Reflection</h2>
+
+             <p>
+             Through assignment 2, I was able to deploy a website for the first time with the help of my tutors and therefore able to have a bit of understanding in regards to react. I went through all the videos provided and things were starting to make sense. I built the prototype of my website. I still had a lot of hiccups along the way and I had to do extensive research. However assignment 2 proved to be overwhelming because of the workload. I essentially had to do assignment 1 and 2 in one go. 
+             </p>
+
+             <p>
+             I realized that I was working hard and not smart, I wanted to check off everything off of the brief and realized I could not.  Because I was actually coding for the assignment I faced some technical issues. I wanted an animation on my homepage/landing page. The animation I have right now is not the idea I had, I plan to rectify this on my next assignment. 
+             </p> 
+             <p> 
+             And for my internet artwork, I have exactly what I want in terms of being able to display the images closely but because I am working with images that are not mine. I need to reference and mention the owners. My plan is to add the reference to the website of which the image is from when you view it. I am still trying to work around that. My prototype is also a small percentage of what the ideal internet artwork will look like but I am confident in achieving what I want to do after planning it what on paper. 
+             </p>
+
+             <p> 
+             I also spent an awful amount creating tabs for the website’s blogs. I’ve always thoughts tabs work well when arranging a lot of content neatly. This was my first time using “States”. In conclusion assignment 2 taught me a lot and I got the opportunity to catch up with my classmates even though the assignment is incomplete.  
+             </p>
           </div>
   
           <div
             className={toggleState === 3 ? "content  active-content" : "content"}
           >
-            <h2>Content 3</h2>
+            <h2>Proposal of my Internet Artwork</h2>
             <hr />
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed
-              nostrum rerum laudantium totam unde adipisci incidunt modi alias!
-              Accusamus in quia odit aspernatur provident et ad vel distinctio
-              recusandae totam quidem repudiandae omnis veritatis nostrum
-              laboriosam architecto optio rem, dignissimos voluptatum beatae
-              aperiam voluptatem atque. Beatae rerum dolores sunt.
+            The idea around my internet artwork was to showcase the low exposure aesthetic trend. The low exposure aesthetic is a trend that has taken over social media. Across all social media platforms content creators have switched to an edit where they decrease the exposure in their content. This type of editing has been seen more and more often in the year of 2022. 
             </p>
+            <section>
+                <img src={require('../Img/lowexposureaesthetic.jpg')} height={400} width={800}/>
+            </section>
+            <p>
+            I decided to present the trend using pictures but I did not want to just paste the pictures on a website page, I wanted to present the trend in a creative way. Still better than just lining up pictures, a collage or a slideshow would have done the trick. But I wasn’t sure if that would be enough for the assignment. 
+            </p> 
+
+            <p>
+            I have decided to create a mosaic picture that makes up the Facebook logo. I will strategically place the low exposure pictures so that they make up the letter F. The low exposure aesthetic is a trend, a social media trend. I wanted an icon that would represent social media and because Facebook owns most of the social media platforms I decided to go with the Facebook logo. 
+            </p>
+
+        <section>
+        <img src={require('../Img/facebookmosaic.jpg')} height={200} width={600}/>
+        <img src={require('../Img/ff.png')} height={400} width={600}/>
+        </section>
+        <p>
+        On the page they’ll also be music/commentary that you can play in the background while viewing all the images for possible immersion.  
+        </p>
+
+        <h2>Description of my prototype</h2>
+        <p>
+        My prototype is a few pictures of the low exposure aesthetic, I’ve demonstrated how the images are going to be viewed from the mosaic. The columns have been created. I just need to start adding the pictures strategically. 
+        </p>
           </div>
         </div>
       </div>
